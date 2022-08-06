410483d623f078c178666b36fa68d8ea2eee3e76
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-08-06 23:20:01 +0900

    ダイアログサンプルとteleport

--- src/components/Card/CardList.vue
+++ src/components/Card/CardList.vue
@@ -3,7 +3,7 @@ import { ref, computed } from "vue";
 import FirstCardList from "./FirstCardList.vue";
 import SecondCardList from "./SecondCardList.vue";
 
-const isFirstTab = ref(true);
+const isFirstTab = ref<boolean>(true);
 const udateTab = (isFirst: boolean) => {
   isFirstTab.value = isFirst;
 };
@@ -11,12 +11,18 @@ const udateTab = (isFirst: boolean) => {
 const currentComponent = computed(() => {
   return isFirstTab.value ? FirstCardList : SecondCardList;
 });
+
+const isDialogOpen = ref<boolean>(false);
+const toggleDialog = () => {
+  isDialogOpen.value = !isDialogOpen.value;
+};
 </script>
 
 <template>
   <div class="tab-changer">
     <button @click="udateTab(true)">First</button>
     <button @click="udateTab(false)">Second</button>
+    <button @click="toggleDialog()">dialog</button>
   </div>
   <div class="tab-contents">
     <FirstCardList v-if="isFirstTab" />
@@ -27,6 +33,14 @@ const currentComponent = computed(() => {
   <div class="tab-contents">
     <component :is="currentComponent"></component>
   </div>
+
+  <teleport to="body">
+    <dialog class="dialog" :open="isDialogOpen">
+      <h1>Dialog</h1>
+      <p>This is a dialog</p>
+      <button @click="toggleDialog">Close</button>
+    </dialog>
+  </teleport>
 </template>
 
 <style scoped>
@@ -34,5 +48,19 @@ const currentComponent = computed(() => {
   display: flex;
   justify-content: center;
   flex-direction: row;
+}
+
+.dialog {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 300px;
+  height: 150px;
+  background-color: rgba(230, 230, 200, 0.8);
+  border-color: grey;
+  border-radius: 16px;
+  align-items: center;
 }
 </style>
