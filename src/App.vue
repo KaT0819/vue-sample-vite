62f1768db7f2ced6b9affabdc09f3cc0bac5cb42
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-08-06 15:55:29 +0900

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Payment from './components/Payment/Payment/Payment.vue'
-import Tweet from './components/Tweet/Tweet.vue'
-import Person from './components/Person/Persons.vue'
+import Payment from "./components/Payment/Payment/Payment.vue";
+import Tweet from "./components/Tweet/Tweet.vue";
+import Person from "./components/Person/Persons.vue";
+import CardList from "./components/Card/CardList.vue";
 </script>
 
 <template>
   <!-- <Payment /> -->
   <!-- <Tweet /> -->
-  <Person />
+  <!-- <Person /> -->
+  <CardList />
 </template>
 
 <style>
